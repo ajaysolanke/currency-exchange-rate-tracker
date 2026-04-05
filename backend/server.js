bcd7058ef356930ea96ff89f28d53b7e8bd9a511
Ajay Solanke <tech@TEKKRs-MacBook-Air.local>
2026-04-05 13:11:08 +0530

    support PORT env var for backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // In-memory storage for API configs
 const apiConfigs = {
